e9fafdc984a12a6097d50a11dee75ce3b72e324e
lucianpopescu688 <lucianpopescu688@gmail.com>
2025-03-06 10:18:40 +0200

    Upload project

--- TestingStuff/TestingPlay.py
+++
@@ -1,7 +0,0 @@
-from model.Player import Player
-from model.hack.bruteForce.BruteForce import BruteForce
-
-
-def testing():
-    p = Player("myNameHere")
-    bruteAttack = BruteForce("Brute force", 0, 0, 0)
